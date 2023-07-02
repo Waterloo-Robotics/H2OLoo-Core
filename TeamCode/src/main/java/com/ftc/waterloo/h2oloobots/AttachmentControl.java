f6adc1547fa72eb9fe9b4436ec440bf8babf6d85
AlphaRant9 <ethan.putz3.14@gmailc.om>
2023-07-01 19:04:18 -0500

    Reworked Core libraries to be more user friendly, will eventually add turning by degrees or radians to the OdometryControl class

--- TeamCode/src/main/java/com/ftc/waterloo/h2oloobots/AttachmentControl.java
+++ TeamCode/src/main/java/com/ftc/waterloo/h2oloobots/AttachmentControl.java
@@ -16,13 +16,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class AttachmentControl {
 
-    // This declares the variables for a color sensor, a distance sensor, a touch sensor, a motor, and a servo.
-    public static ColorSensor color;
-    public static DistanceSensor distance;
-    public static TouchSensor touch;
-    public static DcMotor motor;
-    public static Servo servo;
-
     // This declares global variables to make our lives easier, we set these when we initialise the file.
     TelemetryControl telemetryControl;
     HardwareMap hardwareMap;
@@ -32,24 +25,11 @@ public class AttachmentControl {
         this.hardwareMap = hardwareMap;
         this.telemetryControl = telemetryControl;
 
-//        color = hardwareMap.colorSensor.get("color");
-//        distance = hardwareMap.get(DistanceSensor.class, "distance");
-        touch = hardwareMap.touchSensor.get("touch");
-        motor = hardwareMap.dcMotor.get("motor");
-        servo = hardwareMap.servo.get("servo");
-
     }
-
-
 
     public void attachmentTelemetry() {
 
-        telemetryControl.addData("Color Sensor - Red", color.red());
-        telemetryControl.addData("Color Sensor - Green", color.green());
-        telemetryControl.addData("Color Sensor - Blue", color.blue());
-        telemetryControl.addData("Color Sensor - Distance (Inches)", ((DistanceSensor) color).getDistance(DistanceUnit.INCH));
-        telemetryControl.addData("Distance Sensor - Distance (Inches)", distance.getDistance(DistanceUnit.INCH));
-        telemetryControl.addData("Touch Sensor Touched", touch.isPressed());
+
 
     }
 
---
+++ TeamCode/src/main/java/com/ftc/waterloo/h2oloobots/Encoder.java
@@ -0,0 +1,108 @@
+package com.ftc.waterloo.h2oloobots;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Encoder {
+
+    DcMotorEx encoder;
+
+    double countsPerRevolution;
+    double wheelDiameter;
+    double wheelCircumference;
+
+    public enum MeasurementUnit {
+
+        IN,
+        MM
+
+    }
+
+    MeasurementUnit measurementUnit;
+
+    public Encoder(HardwareMap hardwareMap, String deviceName, double countsPerRevolution, double wheelDiameter, MeasurementUnit measurementUnit) {
+
+        this.encoder = (DcMotorEx) hardwareMap.dcMotor.get(deviceName);
+        encoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        encoder.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        this.countsPerRevolution = countsPerRevolution;
+        this.wheelDiameter = wheelDiameter;
+        this.wheelCircumference = wheelDiameter * Math.PI;
+        this.measurementUnit = measurementUnit;
+
+    }
+
+    public void setDirection(DcMotorSimple.Direction direction) {
+
+        encoder.setDirection(direction);
+
+    }
+
+    public void setMode(DcMotor.RunMode mode) {
+
+        encoder.setMode(mode);
+
+    }
+
+    public void setTargetPosition(int position) {
+
+        encoder.setTargetPosition(position);
+
+    }
+
+    public void setTargetPositionTolerance(int tolerance) {
+
+        encoder.setTargetPositionTolerance(tolerance);
+
+    }
+
+    public int getCurrentPosition() {
+
+        return encoder.getCurrentPosition();
+
+    }
+
+    public double getRotations() {
+
+        return this.getCurrentPosition() / countsPerRevolution;
+
+    }
+
+    public double getDistance() {
+
+        return this.getRotations() * this.wheelCircumference;
+
+    }
+
+    public double getDistance(MeasurementUnit measurementUnit) {
+
+        double distance = 0;
+
+        if (measurementUnit != this.measurementUnit) {
+
+            switch (measurementUnit) {
+
+                case IN:
+                    distance = this.getDistance() / 25.4;
+                    break;
+
+                case MM:
+                    distance = this.getDistance() * 25.4;
+                    break;
+
+            }
+
+        } else {
+
+            distance = this.getDistance();
+
+        }
+
+        return distance;
+
+    }
+
+}
