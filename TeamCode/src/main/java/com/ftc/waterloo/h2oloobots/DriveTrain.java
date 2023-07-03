4760c6d21765acaedc667044fc655b881e1f5780
AlphaRant9 <ethan.putz3.14@gmailc.om>
2023-07-03 00:32:02 -0500

    More documentation, made changes to DriveTrain

--- TeamCode/src/main/java/com/ftc/waterloo/h2oloobots/DriveTrain.java
+++ TeamCode/src/main/java/com/ftc/waterloo/h2oloobots/DriveTrain.java
@@ -3,32 +3,126 @@ package com.ftc.waterloo.h2oloobots;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class DriveTrain {
 
-    public DcMotor fl, fr, bl, br;
+    public enum DriveTrainType {
 
-    public DcMotor left, right;
+        TWO_WHEEL_DRIVE,
+        FOUR_WHEEL_TANK,
+        MECANUM
 
-    double COUNTS_PER_INCH;
+    }
 
-    double COUNTS_PER_DEGREE;
+    DriveTrainType driveTrainType;
 
+    // defines drive motors for a 4 wheel drive
+    DcMotor fl, fr, bl, br;
+
+    // defines drive motors for a two wheel drive
+    DcMotor left, right;
+
+    /* counts per revolution of the drive motors. None of this is necessary if you aren't using
+     * the encoder drive in this file. */
+    double countsPerRevolution = 553;
+
+    // diameter of your drive wheels
+    double wheelDiameter = 96 / 25.4; // inches
+    double wheelCircumference = wheelDiameter * Math.PI;
+
+    /*
+     * Track Width is the distance between the two sets of wheels (defined by the line of x below).
+     *
+     * TODO this will need to be updated for turns to work. it might not be the actual value
+     *  that is physically measured.
+     *
+     *          FRONT
+     *  O--------------------O
+     *  |                    |
+     *  |                    |
+     *  |                    |
+     *  |xxxxxxxxxxxxxxxxxxxx|
+     *  |                    |
+     *  |                    |
+     *  |                    |
+     *  O--------------------O
+     */
+    double trackWidth = 1.0;
+    // how many inches the wheels travel in a full rotation of the robot
+    double fullRotation = trackWidth * Math.PI;
+
+    // calculates how many encoder counts are in an inch and in a degree
+    double COUNTS_PER_INCH = countsPerRevolution / wheelCircumference;
+    double COUNTS_PER_DEGREE = fullRotation / wheelCircumference * countsPerRevolution / 360.0;
+
+    // defines local HardwareMap and TelemetryControl variables.
     HardwareMap hardwareMap;
-
     TelemetryControl telemetryControl;
 
-    public DriveTrain(HardwareMap hardwareMap, TelemetryControl telemetryControl) {
+    /**Initialises the drivetrain variable.
+     * @param hardwareMap the local HardwareMap variable from in the runOpMode() void.
+     * @param telemetryControl the TelemetryControl variable initialized in the runOpMode() void.
+     * @param driveTrainType the drivetrain type: can be two wheel, 4 wheel tank, or mecanum.*/
+    public DriveTrain(HardwareMap hardwareMap,
+                      TelemetryControl telemetryControl,
+                      DriveTrainType driveTrainType
+    ) {
 
         this.hardwareMap = hardwareMap;
         this.telemetryControl = telemetryControl;
+        this.driveTrainType = driveTrainType;
+
+        switch (driveTrainType) {
+
+            case TWO_WHEEL_DRIVE:
+                this.TwoWheelInit();
+                break;
+
+            case FOUR_WHEEL_TANK:
+            case MECANUM:
+                this.FourMotorInit();
+                break;
+
+        }
+
+    }
+
+    /**Initialises the drivetrain variable.
+     * @param hardwareMap the local HardwareMap variable from in the runOpMode() void.
+     * @param telemetryControl the TelemetryControl variable initialized in the runOpMode() void.
+     * @param driveTrainType the drivetrain type: can be two wheel, 4 wheel tank, or mecanum.
+     * @param zeroPowerBehavior the zero power behavior to be set to the drive motors.*/
+    public DriveTrain(
+            HardwareMap hardwareMap,
+            TelemetryControl telemetryControl,
+            DriveTrainType driveTrainType,
+            DcMotor.ZeroPowerBehavior zeroPowerBehavior
+    ) {
+
+        this.hardwareMap = hardwareMap;
+        this.telemetryControl = telemetryControl;
+        this.driveTrainType = driveTrainType;
+
+        switch (driveTrainType) {
+
+            case TWO_WHEEL_DRIVE:
+                this.TwoWheelInit(zeroPowerBehavior);
+                break;
+
+            case FOUR_WHEEL_TANK:
+            case MECANUM:
+                this.FourMotorInit(zeroPowerBehavior);
+                break;
+
+        }
 
     }
 
     /**Initialization for two motor drivebase (one left, one right).*/
-    public void TwoWheelInit() {
+    void TwoWheelInit() {
 
         left = hardwareMap.dcMotor.get("left");
         right = hardwareMap.dcMotor.get("right");
@@ -41,18 +135,44 @@ public class DriveTrain {
 
     }
 
-    /**Simple two wheel drive TeleOp.
-     * @param FBInput input used for forward and back movements.
-     * @param PivotInput input used for turning.*/
-    public void TwoWheelDriveTeleOp(double FBInput, double PivotInput) {
+    /**Initialization for two motor drivebase (one left, one right).
+     * @param zeroPowerBehavior the zero power behavior to set to the drive motors.*/
+    void TwoWheelInit(DcMotor.ZeroPowerBehavior zeroPowerBehavior) {
 
-        right.setPower(FBInput - PivotInput);
-        left.setPower(-FBInput - PivotInput);
+        left = hardwareMap.dcMotor.get("left");
+        right = hardwareMap.dcMotor.get("right");
+
+        left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        left.setZeroPowerBehavior(zeroPowerBehavior);
+        right.setZeroPowerBehavior(zeroPowerBehavior);
 
     }
 
-    /**Four motor initialization command for any four motor drive base with two motors on either side. Could be mecanum or tank.*/
-    public void FourMotorInit(DcMotor.ZeroPowerBehavior zeroPowerBehavior) {
+    /**Four motor initialization command for any four motor drive base with two motors on either
+     * side. Could be mecanum or tank.*/
+    void FourMotorInit() {
+
+        fl = hardwareMap.dcMotor.get("fl");
+        fr = hardwareMap.dcMotor.get("fr");
+        bl = hardwareMap.dcMotor.get("bl");
+        br = hardwareMap.dcMotor.get("br");
+
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }
+
+    /**Four motor initialization command for any four motor drive base with two motors on either
+     * side. Could be mecanum or tank.
+     * @param zeroPowerBehavior the zero power behavior to set to the motors.*/
+    void FourMotorInit(DcMotor.ZeroPowerBehavior zeroPowerBehavior) {
 
         fl = hardwareMap.dcMotor.get("fl");
         fr = hardwareMap.dcMotor.get("fr");
@@ -76,17 +196,53 @@ public class DriveTrain {
 
     }
 
-    public void FourMotorInit() {
+    public void teleOpDrive(double FBInput, double pivotInput) {
 
-        fl = hardwareMap.dcMotor.get("fl");
-        fr = hardwareMap.dcMotor.get("fr");
-        bl = hardwareMap.dcMotor.get("bl");
-        br = hardwareMap.dcMotor.get("br");
+        switch (driveTrainType) {
 
-        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            case TWO_WHEEL_DRIVE:
+                this.TwoWheelDriveTeleOp(FBInput, pivotInput);
+                break;
+
+            case FOUR_WHEEL_TANK:
+                this.FWDTeleOp(FBInput, pivotInput);
+                break;
+
+            case MECANUM:
+                RobotLog.setGlobalErrorMsg("Passing in a LRInput is necessary. Please use the " +
+                        "version of this function that uses 'teleOpDrive(FBInput, LRInput," +
+                        " pivotInput)'.");
+
+        }
+
+    }
+
+    public void teleOpDrive(double FBInput, double LRInput, double pivotInput) {
+
+        switch (driveTrainType) {
+
+            case TWO_WHEEL_DRIVE:
+            case FOUR_WHEEL_TANK:
+                RobotLog.setGlobalErrorMsg("Passing in a LRInput is unacceptable. Please use the " +
+                        "version of this function that uses 'teleOpDrive(FBInput," +
+                        " pivotInput)'.");
+                break;
+
+            case MECANUM:
+                this.MecanumTeleOp(FBInput, LRInput, pivotInput);
+                break;
+
+        }
+
+    }
+
+    /**Simple two wheel drive TeleOp.
+     * @param FBInput input used for forward and back movements.
+     * @param PivotInput input used for turning.*/
+    public void TwoWheelDriveTeleOp(double FBInput, double PivotInput) {
+
+        right.setPower(FBInput - PivotInput);
+        left.setPower(-FBInput - PivotInput);
 
     }
 
@@ -95,7 +251,7 @@ public class DriveTrain {
     double bldir = 0;
     double brdir = 0;
 
-    public void FWDTeleOp(double FBInput, double PivotInput, boolean RUN_USING_ENCODER) {
+    void FWDTeleOp(double FBInput, double PivotInput) {
 
         fr.setPower(-FBInput - PivotInput);
         fl.setPower(FBInput - PivotInput);
@@ -106,7 +262,7 @@ public class DriveTrain {
 
     double speedMul = 1;
 
-    public void MecanumTeleOp(double FBInput, double LRInput, double PivotInput) {
+    void MecanumTeleOp(double FBInput, double LRInput, double PivotInput) {
 
         fr.setPower(speedMul * (-FBInput - LRInput - (PivotInput)));
         br.setPower(speedMul * (-FBInput + LRInput - (PivotInput)));
@@ -117,7 +273,7 @@ public class DriveTrain {
 
     }
 
-    public void EncoderAutoInit(double WHEEL_DIAMETER_MM, double GEAR_RATIO, double COUNTS_PER_REVOLUTION) {
+    void EncoderAutoInit(double WHEEL_DIAMETER_MM, double GEAR_RATIO, double COUNTS_PER_REVOLUTION) {
 
         double WHEEL_DIAMETER_INCHES = WHEEL_DIAMETER_MM / 25.4;
 
@@ -127,7 +283,7 @@ public class DriveTrain {
 
     }
 
-    public void EncoderAutoInit(double GEAR_RATIO, double COUNTS_PER_REVOLUTION) {
+    void EncoderAutoInit(double GEAR_RATIO, double COUNTS_PER_REVOLUTION) {
 
         double WHEEL_DIAMETER_INCHES = 96 / 25.4;
 
@@ -137,7 +293,7 @@ public class DriveTrain {
 
     }
 
-    public void timeAutoMecanumDrive(double FRPower, double FLPower, double BRPower, double BLPower, double SECONDS) {
+    void timeAutoMecanumDrive(double FRPower, double FLPower, double BRPower, double BLPower, double SECONDS) {
 
         ElapsedTime time = new ElapsedTime();
 
@@ -159,7 +315,7 @@ public class DriveTrain {
 
     }
 
-    public void EncoderAutoMecanumDrive(double INCHES_FB, double INCHES_LR, double DEGREES_TURN, double SPEED, int time) {
+    void EncoderAutoMecanumDrive(double INCHES_FB, double INCHES_LR, double DEGREES_TURN, double SPEED, int time) {
 
         ElapsedTime timer = new ElapsedTime();
 
