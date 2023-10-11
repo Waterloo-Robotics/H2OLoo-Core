9e219607e403898625a74b4e4d8920603ac72586
H2OLooBots <ftcteam7247@gmail.com>
2023-10-11 18:14:17 -0500

    Added CameraControl

--- FtcRobotController/src/main/java/com/ftc/waterloo/h2oloobots/H2OLooAuto.java
+++ FtcRobotController/src/main/java/com/ftc/waterloo/h2oloobots/H2OLooAuto.java
@@ -10,11 +10,14 @@ public abstract class H2OLooAuto extends LinearOpMode {
     public OdometryControl odometryControl;
     public AttachmentControl attachmentControl;
 
+    public CameraControl cameraControl;
+
     public void runOpMode() {
 
         telemetryControl = new TelemetryControl(telemetry);
         odometryControl = new OdometryControl(hardwareMap, telemetryControl);
         attachmentControl = new AttachmentControl(hardwareMap, telemetryControl, gamepad1, gamepad2);
+        cameraControl = new CameraControl(hardwareMap, telemetryControl);
 
         this.opModeInit();
 
